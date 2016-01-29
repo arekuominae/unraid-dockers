d5cbfb5361b4c92ae9dc934c485ebc19fed7e602
andrew <andrew@codechimp.org>
2016-01-29 12:41:54 +0000

    Replace IP on start

--- elk/init_elk.sh
+++ elk/init_elk.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+IP="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+
 if [ ! -d "/config/logstash/" ]; then
   mkdir -p /config/logstash
   chown -R nobody:users /config/logstash
@@ -22,5 +24,9 @@ fi
 
 # Move the default conf file (if not exists)
 mv -n /root/logstash.conf /config/logstash/
+
+# Edit the config files to change localhost to the current IP
+sed -i "s/hosts =>.*/hosts => ["\"${IP}:9200\""]/g" /config/logstash/logstash.conf
+sed -i "s/elasticsearch_url.*/elasticsearch_url: \"http:\/\/${IP}:9200\"/g" /kibana/config/kibana.yml
 
 chmod -R 0777 /config
