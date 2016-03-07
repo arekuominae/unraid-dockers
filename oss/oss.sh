504301406014d4d2c42b2720c3feca11d69c7105
Andrew-Codechimp <andrew@codechimp.org>
2016-03-07 15:15:14 +0000

    Minor mods

--- oss/oss.sh
+++ oss/oss.sh
@@ -5,4 +5,5 @@ MEMORY=${MEMORY:-1512m}
 # always stops service
 service opensearchserver stop
 sed -e 's/#JAVA_OPTS="-Xms1G -Xmx1G"/JAVA_OPTS="-Xms'$MEMORY' -Xmx'$MEMORY'"/' -i /etc/opensearchserver
+tail -F /var/log/opensearchserver/server.out
 service opensearchserver start
